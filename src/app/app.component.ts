991a35338c651e5cbfee6385eb3446460be198bb
adekams <kifademi@gmail.com>
2021-10-11 08:22:52 +0100

    app cleanup, components generated, semantic-ui installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'currency-converter';
+  // https://free.currconv.com/api/v7/convert?q=USD_NGN&compact=ultra&apiKey=c694e8c527f28ee28c46
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './components/button/button.component';
+import { FormComponent } from './components/form/form.component';
+import { ConverterComponent } from './components/converter/converter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    FormComponent,
+    ConverterComponent
   ],
   imports: [
     BrowserModule,
