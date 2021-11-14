ca9283d291d2d9d3ee6a5c395acd85af7aa4a50b
adekams <kifademi@gmail.com>
2021-11-14 12:31:47 +0100

    converter add

--- src/app/components/converter/converter.component.ts
+++ src/app/components/converter/converter.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-converter',
   templateUrl: './converter.component.html',
-  styleUrls: ['./converter.component.css']
+  styleUrls: ['./converter.component.css'],
 })
 export class ConverterComponent implements OnInit {
-
-  constructor() { }
+  constructor(private toastr: ToastrService) {}
 
   ngOnInit(): void {
+    this.showSuccess();
   }
-
+  showSuccess() {
+    this.toastr.success('Hello world!', 'Toastr fun!');
+  }
 }
